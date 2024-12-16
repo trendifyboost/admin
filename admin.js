824c3cbf08bafc683b2f6bce792446353208c47c
Nasimul Islam <125240671+trendifyboost@users.noreply.github.com>
2024-12-16 23:38:43 +0600

    admin.js

--- admin.js
+++ admin.js
@@ -187,15 +187,6 @@ function generateReceiptPDF() {
 
   }).catch(error => console.error("Error viewing customer:", error));
 };
-function showModal() {
-    const modal = document.querySelector(".modal");
-    modal.style.display = "flex"; // Use flex to center it properly
-}
-
-function closeModal() {
-    const modal = document.querySelector(".modal");
-    modal.style.display = "none";
-}
 
 // Initial load
 loadCustomers();
